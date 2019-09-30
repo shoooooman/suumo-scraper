96616d2a9a294b84551dffc45311072660e6c68f
shoman <shoma216@gmail.com>
2019-10-01 00:23:14 +0200

    scrape all pages

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -22,17 +23,31 @@ type Room struct {
 	Area  string `json:"area"`
 }
 
-func main() {
-	url := "https://suumo.jp/chintai/tokyo/ek_27580/"
+func getPages(url string) int {
 	doc, err := goquery.NewDocument(url)
 	if err != nil {
 		panic(err)
 	}
 
-	title := doc.Find("title").Text()
-	fmt.Println(title)
+	// 最後のページを取得
+	// titleから取得しても良い
+	pagesStr := doc.Find("div.pagination.pagination_set-nav > ol > li").Last().Text()
+	pages, _ := strconv.Atoi(pagesStr)
+
+	return pages
+}
+
+func scrapePage(baseURL string, page int) []Building {
+	pageStr := strconv.Itoa(page)
+	url := baseURL + "?page=" + pageStr + "&rn=0020"
+
+	doc, err := goquery.NewDocument(url)
+	if err != nil {
+		panic(err)
+	}
 
 	buildings := []Building{}
+
 	// 建物を全て取得
 	selection := doc.Find("div.cassetteitem")
 	selection.Each(func(index int, s *goquery.Selection) {
@@ -50,12 +65,10 @@ func main() {
 
 		// 築年数と高さ
 		col3 := s.Find("li.cassetteitem_detail-col3 > div")
-		col3.Each(func(_ int, sc *goquery.Selection) {
-			age := sc.First().Text()
-			height := sc.Last().Text()
-			building.Age = age
-			building.Height = height
-		})
+		age := col3.First().Text()
+		height := col3.Last().Text()
+		building.Age = age
+		building.Height = height
 
 		// 部屋を取得
 		rselection := s.Find("div.cassetteitem-item > table > tbody")
@@ -73,9 +86,26 @@ func main() {
 		buildings = append(buildings, building)
 	})
 
+	return buildings
+}
+
+func main() {
+	baseURL := "https://suumo.jp/chintai/tokyo/ek_27580/"
+
+	pages := getPages(baseURL)
+	fmt.Println(pages)
+
+	buildings := []Building{}
+	// ページ毎の情報を取得し連結
+	for i := 1; i <= pages; i++ {
+		info := scrapePage(baseURL, i)
+		buildings = append(buildings, info...)
+	}
+
 	jsonBytes, err := json.Marshal(&buildings)
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println(string(jsonBytes))
+
 }
